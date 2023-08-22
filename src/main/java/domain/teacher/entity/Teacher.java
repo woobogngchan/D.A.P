12ed4c1afb73fd7e0592ad8a179a3a18f6a471a8
Chanhyeok <kimmi07091@gmail.com>
2023-08-22 17:22:53 +0900

    Docs: Teacher

--- src/main/java/domain/teacher/entity/Teacher.java
+++ src/main/java/domain/teacher/entity/Teacher.java
@@ -1,4 +1,47 @@
 package domain.teacher.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String teacherName;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @Column
+    private int age;
+
+    @Column(nullable = false)
+    private String janre;
+
+    @Column
+    private String originalTeacherImage;
+
+    @Column
+    private Long teacherImageSize;
+
+    @Column
+    private String teacherImage;
+
+    public Teacher(String teacherName, String nickName, int age, String janre, String originalTeacherImage, Long teacherImageSize, String teacherImage) {
+        this.teacherName = teacherName;
+        this.nickName = nickName;
+        this.age = age;
+        this.janre = janre;
+        this.originalTeacherImage = originalTeacherImage;
+        this.teacherImageSize = teacherImageSize;
+        this.teacherImage = teacherImage;
+    }
 }
